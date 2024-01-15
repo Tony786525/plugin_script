7c3e4e0e22a124397163ff2409f6ad8c728b0fd5
Tony786525 <146442695+Tony786525@users.noreply.github.com>
2024-01-15 12:24:11 -0300

    create_1st_plugin.lua

--- create_1st_plugin.lua
+++ create_1st_plugin.lua
@@ -3,5 +3,7 @@ local ScriptPlugin = plugin:CreateToolbar("toolbar name")
 local button = ScriptPlugin:CreateButton("plugin name","description","rbxassetid://")
 
 button.Click:Connect(function()
-		
+	-- here is the code you will put
+	
 end)
+-- put this in a script.
